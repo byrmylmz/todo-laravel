1962d9c36c59342681d883cb134d588f378d9f1a
bayram <68708205+byrmylmz@users.noreply.github.com>
2021-12-17 19:37:37 +0300

    test

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,7 @@ Route::middleware('auth:api')->group(function(){
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+    
     Route::get('/todos',[TodosController::class,'index']);
     Route::post('/todos',[TodosController::class,'store']);
     Route::patch('/todos/{todo}',[TodosController::class,'update']);
